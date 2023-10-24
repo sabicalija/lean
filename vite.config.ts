c812344e72341a057d775b757fa611fd869c365e
Alija Sabic <sabic.alija@gmail.com>
2023-10-24 23:34:48 +0200

    Update vite configuration: use constants, define alias, provide configuration for local vite development server

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
+import path from "path";
+import fs from "fs";
+import constants from "./src/constants.json";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: constants.BASE_PATH,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [vue()],
-  base: "/tracker/",
+  server: {
+    https: {
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
+    },
+    host: "localhost",
+    port: 3000,
+    strictPort: true,
+  },
 });
