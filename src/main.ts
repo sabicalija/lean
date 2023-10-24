7bc1e2ced72ecb473592f539246cca68914dbef9
Alija Sabic <sabic.alija@gmail.com>
2023-10-24 23:35:52 +0200

    Update vue setup: add router, store, authentication with firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./plugins/router.ts";
+import { setupStore, useUserStore } from "./plugins/store.ts";
+import { auth } from "./plugins/firebase.ts";
+import "./style.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+setupStore(app);
+
+const userStore = useUserStore();
+auth.onAuthStateChanged((user) => {
+  userStore.setUser(user);
+});
+
+app.mount("#app");
