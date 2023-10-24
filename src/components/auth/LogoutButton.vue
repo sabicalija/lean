d738f4f94ac06a1d792362227b866bfd4b712c08
Alija Sabic <sabic.alija@gmail.com>
2023-10-24 23:36:50 +0200

    Add first components: OAuthButtons, LogoutButton

---
+++ src/components/auth/LogoutButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="logout">Logout</button>
+</template>
+
+<script lang="ts">
+import { auth } from "@/plugins/firebase.ts";
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const router = useRouter();
+
+    const logout = async () => {
+      try {
+        await auth.signOut();
+        router.push("/login");
+      } catch (error) {
+        console.error("Logout Failed:", error);
+      }
+    };
+
+    return { logout };
+  },
+};
+</script>
---
+++ src/components/auth/OAuthButtons.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="loginWithGoogle">Login with Google</button>
+    <button @click="loginWithGithub">Login with GitHub</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { auth } from "@/plugins/firebase.ts";
+import { signInWithRedirect, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+
+const loginWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+};
+
+const loginWithGithub = () => {
+  const provider = new GithubAuthProvider();
+  signInWithRedirect(auth, provider);
+};
+</script>
