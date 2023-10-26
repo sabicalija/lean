e59ed4611985bac07095cab64e92eaaf3987c6d2
Alija Sabic <sabic.alija@gmail.com>
2023-10-27 00:50:19 +0200

    Remove all solid fontAwesome icons and include only need ones

--- src/plugins/icons.ts
+++ src/plugins/icons.ts
@@ -1,12 +1,12 @@
 import { App } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   install: (app: App) => {
     // Add the FontAwesome icons to the library
-    library.add(fas);
+    library.add(faBars);
 
     // Register the FontAwesomeIcon component globally
     app.component("font-awesome-icon", FontAwesomeIcon);
