113b50efdb6b333b64404b6e29e4dc59c3ea2024
Alija Sabic <sabic.alija@gmail.com>
2023-11-08 17:13:45 +0100

    Add fontawesome icon 'location'

--- src/plugins/icons.ts
+++ src/plugins/icons.ts
@@ -1,12 +1,12 @@
 import { App } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faLocationArrow } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   install: (app: App) => {
     // Add the FontAwesome icons to the library
-    library.add(faBars);
+    library.add(faBars, faLocationArrow);
 
     // Register the FontAwesomeIcon component globally
     app.component("font-awesome-icon", FontAwesomeIcon);
